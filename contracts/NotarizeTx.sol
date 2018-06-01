688c25be3abe8249142d04af5a74efa5e0ee2d78
Unknown <daniellenarduzzi@gmail.com>
2018-06-01 14:09:49 -0300

    Single contract for multiple tx's
    - Changed version
    - Added constructor
    - Added mapping 
    - Added functions newTransaction, updateStatus
    - Added events newTxEvent, updateStatusEvent

--- contracts/NotarizeTx.sol
+++ contracts/NotarizeTx.sol
@@ -1,70 +1,52 @@
 /*
  Bitsign transaction notarizacion contract v0.1c
 */
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.24;
 
 contract NotarizeTx {
+    address owner;
 
-  //State variables
-    mapping (bytes32 => bytes32) private proofs;
-    address public BSG_NODE;
+    constructor() public {
+      owner = msg.sender;
+    }
+
+    enum Status {purchased, confirmed, shiped, recieved, completed}
+    modifier onlyOwner() {
+      require(msg.sender == owner);
+      _;
+    }
+
     struct Tx {
         address  buyer;
         address  seller;
         bytes32  id;
-        string  date;
+        uint256 date;
         uint  value;
-        bytes32  hash;
-        string  status;
-        string  shipping;
+        bytes32  key;
+        Status  status;
     }
-
-    Tx _tx;
-
-    function getTx() public view returns (address,address,bytes32,string,uint,
-    bytes32,string,string){
-
-        return (_tx.buyer, _tx.seller, _tx.id, _tx.date, _tx.value, _tx.hash,
-        _tx.status, _tx.shipping);
-    }
-
-    event NotaryEvt(bytes32 _hash, bytes32 _id);
-
-  /*
-    Contract constructor takes _user as client Ethereum address
-   */
-    constructor(address _buyer, address _seller, bytes32 _id, string _date,
-       uint _value, bytes32 _hash, string _status, string _shipping) public {
+    /*  mapping id's => Transactions*/
+    mapping (bytes32 => Tx) public idToTx;
+    event newTxEvent(Tx tx);
+    function newTx(address _buyer, address _seller, bytes32 _id, uint256 _date,
+    uint _value, bytes32 _key) public onlyOwner {
+        Tx _tx = idToTx[_id];
         _tx.buyer = _buyer;
         _tx.seller = _seller;
         _tx.id = _id;
-        _tx.date = _date;
+        _tx.date[Status.purchased] = _date;
         _tx.value = _value;
-        _tx.hash = _hash;
-        _tx.status = _status;
-        _tx.shipping = _shipping;
-        proofs[_hash] = _id;
-        BSG_NODE = msg.sender;
+        _tx.key = _key;
+        _tx.status = Status.purchased;
+        proofs[_key] = _id;
+        emit newTxEvent(_tx);
     }
-  /**
-  *
-  *
-   */
-    function updateStatus(string _status, bytes32 _id, bytes32 _hash) public {
-        require (_id == _tx.id && (msg.sender == _tx.buyer || msg.sender == BSG_NODE));
-        _tx.status = _status;
-        _tx.hash = _hash;
-        proofs[_hash] = _id;
-        emit NotaryEvt(_hash, _tx.id);
-    }
-    /**
-    *
-     */
-    function updateShipping(string _shipping, bytes32 _id, bytes32 _hash) public {
-        require(_id == _tx.id && (msg.sender == _tx.buyer || msg.sender == BSG_NODE));
-        _tx.status = _shipping;
-        _tx.hash = _hash;
-        proofs[_hash] = _tx.id;
-        emit NotaryEvt(_hash, _tx.id);
+
+  /* Event trigered when status is updated */
+    event UpdateStatusEvent(Tx tx);
+    /* updateStatus fcn, allows to change status of tx given id */
+    function updateStatus(Status _status, bytes32 _id) public onlyOwner{
+        idToTx[_id].status = _status;
+        emit updateStatusEvent(idToTx[_id]);
     }
 }
